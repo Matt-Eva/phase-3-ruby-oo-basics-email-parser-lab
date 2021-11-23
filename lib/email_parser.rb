1f88409d388db8660f4f0320a9deaaa3b56c5833
Matt Eva <matteweva@gmail.com>
2021-11-23 10:51:27 -0800

    commit tree 96f0b14baf675f31447d7cd55687ebaabe2070a0

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,20 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+    # attr_accessor :
+    def initialize(email_address_string)
+        @addresses = email_address_string
+    end
+
+    def parse
+        address_array = @addresses.split(/, | /)
+        pp address_array.uniq
+        address_array.uniq
+    end
+end
+
+email_addresses = "john@doe.com, person@somewhere.org dorane@gmail.com, Leann@outlook.com, person@somewhere.org"
+parser = EmailAddressParser.new(email_addresses)
+
+parser.parse
